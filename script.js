3b5473a4662ce56daa2d9538a42988c81ea5aa54
Tsm8542 <shaikhabid932@gmail.com>
2023-07-30 17:14:27 +0530

    30-7-2023 05:13 PM

---
+++ script.js
@@ -0,0 +1,99 @@
+function x(){
+    var x = document.querySelector("#topic").value;
+    var t = document.querySelector("#frame")
+    switch(x){
+        case 'boa':
+            t.src = 'Files/boa.html';
+            break;
+        case 'iot':
+            t.src = 'Files/iot.html';
+            break;
+        case 'st':
+            t.src = 'Files/st.html';
+            break;
+        case 'matit':
+            t.src = 'Files/matit.html';
+            break;
+        case 'lagit':
+            t.src = 'Files/lagit.html';
+            break;
+        case 'ccaccit':
+            t.src = 'Files/ccaccit.html';
+            break;
+        case 'bit':
+            t.src = 'Files/bit.html';
+            break;
+        case 'sit':
+            t.src = 'Files/sit.html';
+            break;
+        case 'mcait':
+            t.src = 'Files/mcait.html';
+            break;
+        case 'icit':
+            t.src = 'Files/icit.html';
+            break;
+        case 'aparvit':
+            t.src = 'Files/aparvit.html';
+            break;
+        case 'awcvit':
+            t.src = 'Files/awcvit.html';
+            break;
+        case 'buarit':
+            t.src = 'Files/buarit.html'
+            break;
+        case 'awjvit':
+            t.src = 'Files/awjvit.html'
+            break;
+        case 'mrjaovit':
+            t.src = 'Files/mrjaovit.html'
+            break;
+        case 'imit':
+            t.src = 'Files/imit.html'
+            break;
+        case 'isiit':
+            t.src = 'Files/isiit.html'
+            break;
+        case 'isglit':
+            t.src = 'Files/isglit.html'
+            break;
+        case 'ibwit':
+            t.src = 'Files/ibwit.html'
+            break;
+        case 'ijcit':
+            t.src = 'Files/ijcit.html'
+            break;
+        case 'ippit':
+            t.src = 'Files/ippit.html'
+            break;
+        case 'ispit':
+            t.src = 'Files/ispit.html'
+            break;
+        case 'mbaspit':
+            t.src = 'Files/mbaspit.html'
+            break;
+        case 'tgit':
+            t.src = 'Files/tgit.html'
+            break;
+        case 'vtacit':
+            t.src = 'Files/vtacit.html'
+            break;
+        case 'pit':
+            t.src = 'Files/pit.html'
+            break;
+        case 'piarit':
+            t.src = 'Files/piarit.html'
+            break;
+        case 'bariaecd':
+            t.src = 'Files/bariaecd.html'
+            break;
+        case 'skit':
+            t.src = 'Files/skit.html'
+            break;
+        case 'afp':
+            t.src = 'Files/afp.html'
+            break;
+        default:
+            t.src = 'main.html';
+            break;
+    }
+}
